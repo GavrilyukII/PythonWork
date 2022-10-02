4ffcd6b31c6ddd2f42a1e3ae555aad66e3062d54
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-02 21:11:18 +0300

    выполнена задача 9 + возможные варинты

--- HomeWork/Tasl9.py
+++ HomeWork/Tasl9.py
@@ -3,6 +3,7 @@
 # - 6782 -> 23
 # 0,56 -> 11
 
+# Вариант 1 (моё решение)
 n = (input('Введите вещественное число: '))
 if float(n) < 0:
     n= float(n)* (-1)
@@ -12,3 +13,22 @@ for i in str(n):
         sum+=int(i)
 print (f"{n} -> {sum}")
 
+# Вариант 2 (семинар)
+# number = input ('Введите вещественное число: ')
+# summa = 0
+# for i in number:
+#     if i.isdigit():
+#         summa+=int(i)
+# print (summa)
+
+# Вариант 3 (семинар)
+# number = float(input('Введите вещественное число: '))
+# summa = 0
+# while int(number) != number:
+#     number*=10
+# print (number)
+
+# while number !=0:
+#     summa+=number%10
+#     number//=10
+# print (summa)
