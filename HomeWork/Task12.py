cc3ab19f3b9482111684c37a3d14c45d09810ccc
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-02 23:08:14 +0300

    выполнена задача 12

--- HomeWork/Task12.py
+++ HomeWork/Task12.py
@@ -1,17 +1,21 @@
 # Реализуйте алгоритм перемешивания списка.
 
-from random import shuffle
+# from random import shuffle
+# n = int (input ("Введите размер списка: "))
+# list = []
+# for i in range (n):
+#     list.append(i)
+# print (list)
+# shuffle(list)
+# print (list)
 
+import random
 
-n = int (input ("Введите размер списка: "))
-
-list = []
-for i in range (n):
-    list.append(i)
-
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print (list)
-
-shuffle(list)
-
-print (list)
-
+listA = []
+for i in range (0, len(list)):
+    j = random.randrange(0, len(list))
+    listA.append(list[j])
+    list.remove(list[j])
+print(listA)
