b6c16174ba532745de928ade12ac524c90618ba0
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-02 21:59:44 +0300

    выполнена задача 11

--- HomeWork/Task11.py
+++ HomeWork/Task11.py
@@ -1,7 +1,10 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 
 n = int (input ('Введите число: '))
-list = []
-for i in range (1,n+1):
-    list.append(f'{(1+1/i)**i}')
-print(list)
+def arrayDigit (n):
+    list = []
+    for i in range (1,n+1):
+        list.append((1+1/i)**i)
+    return list
+print(arrayDigit(n))
+print(sum(arrayDigit(n)))
