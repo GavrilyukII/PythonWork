abc667a2f872aca49868efe0723df618cc32ce80
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-02 21:46:46 +0300

    выполнена задача 10 + возможные варинты

--- HomeWork/Task10.py
+++ HomeWork/Task10.py
@@ -2,9 +2,21 @@
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+
+# Вариант 1 (мое решение)
 n = int (input ('Введите число: '))
 fact = 1
 for i in range (1, n+1):
     fact*=i
     print (fact, end=' ')
 
+# Вариант 2 (семинар)
+# def arrayDigitsMltipkier (n):
+#     listA = []
+#     buf = 1
+#     for i in range (1, n+1):
+#         listA.append(i*buf)
+#         buf*=i
+#     return listA
+
+# print (arrayDigitsMltipkier(n))
