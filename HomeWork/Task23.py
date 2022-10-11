a1dd1b3e330b44168b4e87ee7a6ff2e22dfa53d2
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-11 17:55:51 +0300

    выполнена задача 23

---
+++ HomeWork/Task23.py
@@ -0,0 +1,12 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв""
+
+string = 'how quickly the complexity of tasks is growing for example with this absdolbanny it is just terrible'
+print (string)
+find = input("Введите искомый элемент: ")
+
+jobList = string.split(' ')
+print (jobList)
+
+reslist = [x for x in jobList if not find in x]
+
+print (reslist)
