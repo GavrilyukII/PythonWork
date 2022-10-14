7f0c25fac853a489fdb9b0d428dab84cbf173495
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-14 14:14:24 +0300

    выполнено с использованием map и lambda

--- HomeWork/Task11.py
+++ HomeWork/Task11.py
@@ -1,10 +1,14 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 
+# n = int (input ('Введите число: '))
+# # def arrayDigit (n):
+# #     list = []
+# #     for i in range (1,n+1):
+# #         list.append((1+1/i)**i)
+# #     return list
+# # print(arrayDigit(n))
+# # print(sum(arrayDigit(n)))
+
 n = int (input ('Введите число: '))
-def arrayDigit (n):
-    list = []
-    for i in range (1,n+1):
-        list.append((1+1/i)**i)
-    return list
-print(arrayDigit(n))
-print(sum(arrayDigit(n)))
+res = list(range(1, n+1))
+print (list(map(lambda x: (1+1/x)**x, res)))
