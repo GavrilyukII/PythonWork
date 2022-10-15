705df37b5af73c1fd8f90e91716afe5ad24051d2
Gavrilyuk <drugstorecowboy13@mail.ru>
2022-10-15 23:15:58 +0300

    выполнено с использованием list Comprehensions

--- HomeWork/Task1.py
+++ HomeWork/Task1.py
@@ -2,7 +2,11 @@
 # Примеры:
 # - 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 
+# n = int (input('Введите число n: '))
+# # print (n, "->", end = ' ')
+# # for i in range(-n,n+1):
+# #     print(i, end = ', ')
+
 n = int (input('Введите число n: '))
-print (n, "->", end = ' ')
-for i in range(-n,n+1):
-    print(i, end = ', ')
+reslist = [n for n in range (-n,n+1)]
+print (reslist)
